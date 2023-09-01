75f0dff2449060026118f69215a77da11b0292f7
mahirminhajk <kmahirminhaj@gmail.com>
2023-09-01 20:50:47 +0530

    first commit

---
+++ aim1.java
@@ -0,0 +1,38 @@
+
+// Java Programming Code to Find HCF LCM of Two Numbers.
+import java.util.Scanner;
+
+class Find {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the First Number: ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter the Second Number: ");
+        int b = sc.nextInt();
+
+        int hcf = findHCF(a, b);
+        int lcm = findLCM(a, b);
+
+        System.out.println("HCF: " + hcf);
+        System.out.println("LCM: " + lcm);
+
+        sc.close();
+    }
+
+    // Method to find the HCF (GCD) using Euclidean Algorithm
+    public static int findHCF(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return findHCF(b, a % b);
+    }
+
+    // Method to find the LCM using the HCF
+    public static int findLCM(int a, int b) {
+        int hcf = findHCF(a, b);
+        return (a * b) / hcf;
+    }
+
+}
---
+++ aim2.java
@@ -0,0 +1,2 @@
+// Given the sides of a triangle, write a program to check whether the triangle
+// is equilateral, isosceles or scalene and find its area
