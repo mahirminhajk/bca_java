7bab74d33dea924e9eabc6aff9df8a6b2b2cdeb2
mahirminhajk <kmahirminhaj@gmail.com>
2023-09-18 23:37:47 +0530

    added aim6

---
+++ aim6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class BaseConversion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input an integer
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+
+        // Perform base conversions
+        String binary = Integer.toBinaryString(num);
+        String octal = Integer.toOctalString(num);
+        String hexadecimal = Integer.toHexString(num);
+
+        // Display the results
+        System.out.println("Binary: " + binary);
+        System.out.println("Octal: " + octal);
+        System.out.println("Hexadecimal: " + hexadecimal);
+
+        scanner.close();
+    }
+}
